ad9c65f761e219d284caba74c690e1bc7ad56a8e
Mathieu Rene <mathieu.rene@gmail.com>
2021-01-02 14:14:00 -0500

    Add apt-get downloader helper to fetch transitive dependencies

---
+++ apt/download.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+# Downloads packages and their transitive deps into the current directory 
+#
+# Usage: ./download.sh [packages]
+#
+# Inspired from:
+# https://stackoverflow.com/questions/13756800/how-to-download-all-dependencies-and-packages-to-directory
+# https://stackoverflow.com/questions/22008193/how-to-list-download-the-recursive-dependencies-of-a-debian-package
+
+PACKAGES="$@"
+
+mkdir -p ./apt-tmp/
+
+if [ ! -f ./apt-tmp/status ]; then
+  touch apt-tmp/status
+  apt-get -c ./apt.conf update
+fi
+
+ALL_DEPS=$(apt-cache -c ./apt.conf depends --recurse --no-recommends --no-suggests \
+  --no-conflicts --no-breaks --no-replaces --no-enhances \
+  --no-pre-depends ${PACKAGES} | grep "^\w")
+
+echo -e "Downloading:\n ${ALL_DEPS}"
+
+apt-get -c ./apt.conf download ${ALL_DEPS}
